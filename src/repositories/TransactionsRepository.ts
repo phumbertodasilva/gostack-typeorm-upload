f8b4ff9eab8d5f129bdbfb094ee258109dee124b
Paulo Humberto <phumbertodasilva@gmail.com>
2020-05-06 16:25:39 -0300

    - Add Transaction - Valid Category - Add Category - Remove Transaction - Config Upload

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (acc, transactions) => {
+        switch (transactions.type) {
+          case 'income':
+            acc.income += Number(transactions.value);
+            break;
+          case 'outcome':
+            acc.outcome += Number(transactions.value);
+            break;
+          default:
+            break;
+        }
+
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
